856e3766fd982ee1de5786e31a741514b9839168
Artem Kysliakov <kysliakov@gmail.com>
2025-08-13 08:55:28 +0300

    Add files via upload

---
+++ python/lessons_py/Lesson_9/new_choice.py
@@ -0,0 +1,11 @@
+# Практична робота
+
+choice = 32
+if choice < 10:
+    print("Занадто мало!")
+elif choice == 10:
+    print("Все вірно!")
+elif choice > 10:
+    print("Занадто багато!")
+else:
+    print("Щось пішло не так")
