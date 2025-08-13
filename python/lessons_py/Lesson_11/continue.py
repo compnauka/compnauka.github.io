564259f28fc66d88a0ac3f09820e02ed3746d659
Artem Kysliakov <kysliakov@gmail.com>
2025-08-13 08:56:41 +0300

    Add files via upload

---
+++ python/lessons_py/Lesson_11/continue.py
@@ -0,0 +1,8 @@
+# Матеріали уроку
+
+for char in "Minecraft":
+    if char == "c":
+        continue
+    print(char)
+
+print("Where is \"c\"?")
---
+++ python/lessons_py/Lesson_11/friends.py
@@ -0,0 +1,5 @@
+# Практична робота
+
+friends = ["Poul", "Neal", "Jack", "Alex"]
+for friend in friends:
+    print("Yappy New Year", friend)
