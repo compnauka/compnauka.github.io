d01e6082720ce712f5482d8f185f5fb3d95ea7d2
Artem Kysliakov <kysliakov@gmail.com>
2025-08-13 08:57:21 +0300

    Add files via upload

---
+++ python/lessons_py/Lesson_12/perimeter_func.py
@@ -0,0 +1,6 @@
+# Практична робота
+# функція для визначення периметру
+def perimeter (length, width):
+    return 2 * (length + width)
+
+print(perimeter(876, 432))
---
+++ python/lessons_py/Lesson_12/square_func.py
@@ -0,0 +1,6 @@
+# Практична робота
+# функція для визначення площі
+def square (length, width):
+    return length * width
+
+print(square(321, 87))
