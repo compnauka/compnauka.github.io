ce3c612a323f9d0ff2624cb91e62200ad6f4012a
Artem Kysliakov <kysliakov@gmail.com>
2025-08-13 08:54:10 +0300

    Add files via upload

---
+++ python/lessons_py/Lesson_7/picture.py
@@ -0,0 +1,19 @@
+# Матеріали уроку
+import turtle
+
+t = turtle.Pen()
+t.forward(50)
+#turtle.Pen().forward(50)
+
+t.up()
+t.right(90)
+t.forward(100)
+t.down()
+t.left(135)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
