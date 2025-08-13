68763d0ff2a922465939fe85473f8bdca68b314a
Artem Kysliakov <kysliakov@gmail.com>
2025-08-13 08:50:18 +0300

    Add files via upload

---
+++ python/lessons_py/Lesson_2/fine.py
@@ -0,0 +1,4 @@
+hi = '''He sad: "I'm fine"'''
+print(hi)
+hi_new = "He sad: \"I'm fine\""
+print(hi_new)
---
+++ python/lessons_py/Lesson_2/string.py
@@ -0,0 +1,10 @@
+# Практична робота
+
+question = "What is your name?"
+print(question)
+answer = 'My name is Alex'
+print(answer)
+
+poem = '''To be, or not to be,
+that is the question''' 
+print(poem)
