5d07e134c26ce15e366bc52c0c996d10441eb927
Artem Kysliakov <kysliakov@gmail.com>
2025-09-24 21:40:52 +0300

    Update data.js

--- data.js
+++ data.js
@@ -16,7 +16,8 @@ const CATEGORIES = [
     name: "Цифрова грамотність",
     iconClass: "fas fa-laptop", // Іконка для категорії
     services: [
-      { name: "Мишачі перегони", description: "Тренажер для роботи з мишкою.", link: "services/click.html", image: "images/10.jpg", tags: ["2-3 класи"] },
+      { name: "Мишачі перегони", description: "Тренажер для роботи з мишкою.", link: "services/click.html", image: "images/10.jpg", tags: ["1-2 класи"] },
+      { name: "Швидкісні вікна", description: "Практичний тренажер для роботи з вікнами програм.", link: "services/windows.html", image: "images/48.jpg", tags: ["1-2 класи"] },
       { name: "Гарячі клавіши", description: "Інтерактивний урок про роботу з клавіатурою.", link: "services/typing.html", image: "images/18.jpg", tags: ["2-4 класи"] },
       { name: "Спритні пальчики", description: "Веселий тренажер для роботи з клавіатурою.", link: "services/keys.html", image: "images/11.jpg", tags: ["2-4 класи"] },
       { name: "Веселі адреси", description: "Практичний тренажер для введення URL-адрес.", link: "services/urls.html", image: "images/38.jpg", tags: ["3-5 класи"] },
